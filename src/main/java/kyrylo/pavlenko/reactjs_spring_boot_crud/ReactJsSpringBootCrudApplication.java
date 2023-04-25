cffd3ea8210d965a93eccb2c7e31e3a3321c6e74
Kyrylo Pavlenko <kyrylo.pavlenko.w@gmail.com>
2023-04-25 21:14:51 +0300

    Initial commit

---
+++ src/main/java/kyrylo/pavlenko/reactjs_spring_boot_crud/ReactJsSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package kyrylo.pavlenko.reactjs_spring_boot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJsSpringBootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactJsSpringBootCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kyrylo/pavlenko/reactjs_spring_boot_crud/ReactJsSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package kyrylo.pavlenko.reactjs_spring_boot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactJsSpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
